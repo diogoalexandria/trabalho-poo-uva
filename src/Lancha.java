57e91f9c4eb29f2620d1d2d960ce7162cb788b2a
Diogo Alexandria <diogoagrasmsdc@gmail.com>
2018-09-12 15:39:03 -0300

    Atributos, setters e getters da classe Lancha

--- src/Lancha.java
+++ src/Lancha.java
@@ -1,4 +1,11 @@
-
 public class Lancha {
-
+	boolean banheiro;
+	
+	public void setBanheiro(boolean banheiro) {
+		this.banheiro = banheiro;
+	}
+	
+	public boolean getBanheiro() {
+		return banheiro;
+	}
 }
