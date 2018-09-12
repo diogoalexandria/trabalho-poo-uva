2d356a8d4ecc5afcc787bfbfc3b44f608974ea68
Diogo Alexandria <diogoagrasmsdc@gmail.com>
2018-09-12 15:16:43 -0300

    Atributos, getters e setters na classe VeiculosMaritmosTipoCasco

--- src/VeiculosMaritmosTipoCasco.java
+++ src/VeiculosMaritmosTipoCasco.java
@@ -1,4 +1,23 @@
 
 public class VeiculosMaritmosTipoCasco {
-
-}
+	char tipo;
+	String tipoCasco;
+	
+	public void setTipo(char tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void setTipoCasco(String tipoCasco) {
+		this.tipoCasco = tipoCasco;
+	}
+	
+	public char getTipo() {
+		return tipo;
+	}
+	
+	public String getTipoCasco() {
+		return tipoCasco;
+	}	
+}	
+	
+	
