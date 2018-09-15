9eb771ee6202fa5eb6b4070034f151b6c779cf27
Lucas3ms <43039777+Lucas3ms@users.noreply.github.com>
2018-09-15 09:53:21 -0300

    Alteração nas SuperClasse e na Classe Atributo
    Inseri atributos nessas classes

--- TrabalhoPOOA1/src/trabalhopooa1/Motor.java
+++ TrabalhoPOOA1/src/trabalhopooa1/Motor.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabalhopooa1;
 
-/**
- *
- * @author Marcos
- */
 public class Motor {
+    String marca, modelo, tipoCombustivel;
+    int potencia, rpmMax, rpm, numCilindros;
+    
+    
     
 }
